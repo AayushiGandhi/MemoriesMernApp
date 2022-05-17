15ca1b4eb500e80628152d63ee07883e929cb506
AayushiGandhi <ay753882@dal.ca>
2022-05-17 00:13:28 -0300

    get posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,9 @@
 import express from 'express';
+import { getPosts, createPosts } from '../controllers/posts.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('This worksssss')
-});
+router.get('/', getPosts);
+router.get('/', createPosts);
 
 export default router;
